c39a4a880e1f7abd7b54a67b43a4807936e74cdb
Yunjoung808 <s_holmes25@naver.com>
2021-03-25 10:36:00 +0900

    register에서 받은 데이터 암호화/복호화

--- client/src/views/examples/RegisterPage.js
+++ client/src/views/examples/RegisterPage.js
@@ -21,38 +21,33 @@ import {
 } from "reactstrap";
 import UserNavbar from 'components/Navbars/UserNavbar';
 import Footer from "components/Footer/Footer.js";
-import 'pure-react-carousel/dist/react-carousel.es.css';
 import { Link } from "react-router-dom";
-import Axios from "axios";
-import {post} from "axios";
+import { post } from "axios";
 import Caver from "caver-js";
-import GoogleLogin from "components/GoogleLogin/GoogleLogin";
+const Crypto = require('crypto-js');
 
-
-
-const axios = require('axios').default;
 const config = {rpcURL: 'https://api.baobab.klaytn.net:8651'}
 const caver = new Caver(config.rpcURL);
 
-
-
 class RegisterPage extends React.Component {
 
-  state = {
-    squares1to6: "",
-    squares7and8: ""
-  };
+  constructor(props){
+    super(props);
+    this.state = {
+      name:'',
+      email:''
+    }
+  }
+
   componentDidMount() {
     document.body.classList.toggle("register-page");
-
   }
   componentWillUnmount() {
     document.body.classList.toggle("register-page");
-   
   }
 
   addInfo = () => {
-    const url = 'api/info';
+    const url = 'http://localhost:5000/api/info';
     const formData = new FormData();
     formData.append('name', this.state.name);
     formData.append('email', this.state.email);
@@ -60,8 +55,24 @@ class RegisterPage extends React.Component {
     return post(url, formData);
   }
 
+  encrypt(data, key){
+    return Crypto.AES.encrypt(data, key).toString();
+  }
+  
+  decrypt(data, key){
+    return Crypto.AES.decrypt(data, key).toString(Crypto.enc.Utf8);
+  }
+
   handleFormSubmit = (e) => {
     e.preventDefault()
+    
+    let data = this.state.name
+    let key = "hi";
+    let enc = this.encrypt(data, key);
+    console.log("enc : ", enc);
+    let dec = this.decrypt(enc, key);
+    console.log("dec : ", dec);
+
     this.addInfo()
         .then((response)=>{
           console.log(response.data);
@@ -74,71 +85,6 @@ class RegisterPage extends React.Component {
     this.setState(nextStage);
   }
 
-  // constructor(props){
-  //   super(props);
-  //   var params = new URLSearchParams(props.location.search);
-  //   this.state={
-  //     productKey: '',
-  //     brand: '',
-  //     imgUrl: '',
-  //     productName: '',
-  //     dateCreated: '',
-  //     tokenUri:'',
-  //     price:'',
-  //     productKey:null,
-  //     items :[],
-  //     sell_items : [],
-  //     all_items: [],
-  //     allSell_items: [],
-  //     news:{
-  //       id           :'',
-  //       index        :'',
-  //       brand        :'',
-  //       productName  :'',
-  //       image :'',
-  //       tokenUri    :'',
-  //       description  :'',
-  //       price        :'',
-  //       date         :''  
-  //     },
-  //     value:0,min:0,counter:0,
-  //     index:params.get('index')
-  //   };
-
-  constructor(props){
-    super(props);
-    this.state = {
-      name:'',
-      email:''
-    }
-  }
-    
-
-  //   Axios.get("http://localhost:5000/NewP/new/getNewP?index="+params.get('index'))
-  //     .then(response => {
-  //         if(response.status==200){
-  //           this.setState({
-  //             news:response.data[0]
-  //           })
-              
-  //         }else{
-  //             console.log(err);
-  //         }
-  //   })
-  // }
-
-
-
-  handleItemChange = (e, _tokenIndex) => {
-    var tokenIndex=_tokenIndex
-    var itemIndex = this.state.items.findIndex(element => element.Id == tokenIndex)
-    console.log(itemIndex);
-    this.state.items[itemIndex].amount =  e.target.value;
-    this.setState({
-      items :  this.state.items
-    })
-  }
-
   getWallet = () => {
     if (caver.klay.accounts.wallet.length) {
       return caver.klay.accounts.wallet[0]
@@ -149,12 +95,10 @@ class RegisterPage extends React.Component {
     }
   }
 
-
   render() {
     return (  
       <>
         <UserNavbar />
-       
         <div className="wrapper">
           <div className="page-header" style={{display : 'inline'}}>
             <Row className="row-grid justify-content align-items text-left">
@@ -167,14 +111,13 @@ class RegisterPage extends React.Component {
             <Container>
               <Row>
                 <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
-                 
                  <form onSubmit={this.handleFormSubmit}>
                   <Card className="card-register">
                     <CardHeader>
-                    <CardImg
-                      alt="..."
-                      src={require("assets/img/square-purple-1.png")}
-                    />
+                      <CardImg
+                        alt="..."
+                        src={require("assets/img/square-purple-1.png")}
+                      />
                       <CardTitle tag="h4">Register</CardTitle>
                     </CardHeader>
                     <CardBody>
@@ -234,55 +177,47 @@ class RegisterPage extends React.Component {
                       </Form>
                     </CardBody>
                     <CardFooter>
-                      <Button className="btn-round" color="primary" size="lg" type="submit" Link tag={Link} to="/complete-page">
-                        등록하기
-                      </Button>
-                      {/* <Button className="btn-round" color="primary" size="lg" type="submit" >
+                      {/* <Button className="btn-round" color="primary" size="lg" type="submit" Link tag={Link} to="/complete-page">
                         등록하기
                       </Button> */}
+                      <Button className="btn-round" color="primary" size="lg" type="submit" >
+                        등록하기
+                      </Button>
                     </CardFooter>
                   </Card>
                   </form>
                 </Col>
               </Row>
               <div className="register-bg" />
-        
               <div
                 className="square square-2"
                 id="square2"
-              
               />
               <div
                 className="square square-3"
                 id="square3"
-                
               />
               <div
                 className="square square-4"
                 id="square4"
-        
               />
               <div
                 className="square square-5"
                 id="square5"
-         
               />
               <div
                 className="square square-6"
                 id="square6"
-                
               />
               <div
                 className="square square-7"
                 id="square7"
-                
               />
-             
             </Container>
             </div>
             </div>
           </div>
-          <Footer />
+          <Footer/>
       </>
     );
   }
