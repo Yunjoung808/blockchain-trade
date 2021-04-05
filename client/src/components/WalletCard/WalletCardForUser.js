adb9acbab718a37fd9e545d6ce575a1ca0cd7fdd
Yunjoung808 <s_holmes25@naver.com>
2021-04-05 22:17:26 +0900

    fix error in loginUser

--- client/src/components/WalletCard/WalletCardForUser.js
+++ client/src/components/WalletCard/WalletCardForUser.js
@@ -192,9 +192,8 @@ class WalletCardForUser extends React.Component {
         var { keystore, keystoreMsg, keystoreName, accessType } = this.state;
         var walletInstance = this.getWallet();
         const { privateKey } = this.state
-  
+      if(walletInstance){
         return(
-            
           <Card>
           <CardBody>
           <Row>
@@ -221,6 +220,222 @@ class WalletCardForUser extends React.Component {
             <br/>
           </CardBody>
         </Card>
+        )
+      }
+        return(
+            
+        <Card className="card-register">
+            <CardHeader>
+                <CardImg
+                alt="..."
+                src={require("assets/img/square-purple-1.png")}
+                />
+                <CardTitle tag="h4" className="text-neutral">Login for user</CardTitle>
+            </CardHeader>
+            <div className="space-50"></div>
+            <CardBody>
+                <Form className="form" > 
+                    <Row>
+                        <Col className="text-center">
+                        <h2>구글 로그인</h2>
+                        </Col>
+                        <Col className="text-center">
+                        <h2>지갑 계정이 있으신가요?</h2>
+                        </Col>
+                        <Col className="text-center">
+                        <h2>지갑 계정이 없으신가요?</h2>
+                        </Col>
+                    </Row>
+                    <Row>    
+                        <Col className="text-center" ><Login/></Col>
+                        {/* wallet login part */}
+                        <Col className="text-center">
+                            <Button color="success" onClick={() => this.toggleModal("formModal")} > Login </Button>
+                            <Modal
+                            modalClassName="modal-black"
+                            isOpen={this.state.formModal}
+                            toggle={() => this.toggleModal("formModal")}
+                            >
+                            <div className="modal-header justify-content-center">
+                                <Button
+                                className="close"
+                                onClick={() => this.toggleModal("formModal")}
+                                >
+                                <i className="tim-icons icon-simple-remove text-white" />
+                                </Button>
+                                <div className="text-muted text-center ml-auto mr-auto">
+                                <h3 className="mb-0">Login </h3>
+                                </div>
+                            </div>
+                            <div className="modal-body">
+                                <div className="btn-wrapper text-center">
+                                </div>
+                                <Form role="form">
+                                <Fragment>
+                                    {accessType === 'keystore'
+                                    // View 1: Access by keystore + password.
+                                    ? (
+                                        <Fragment>
+                                        <FormGroup className="mb-3">
+                                            <InputGroup
+                                            className={classnames("input-group-alternative", {
+                                                "input-group-focus": this.state.emailFocus
+                                            })}
+                                            >
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                <i className="tim-icons icon-email-85" />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+
+                                            <Input
+                                                placeholder="Keystore"
+                                                id="keystore"
+                                                type="keystore"
+                                                onChange={this.handleImport}
+                                                accept=".json"
+                                                onFocus={e => this.setState({ emailFocus: true })}
+                                                onBlur={e => this.setState({ emailFocus: false })}
+                                            />
+                                            <label className="Auth__button" htmlFor="keystore"></label>
+                                            <input 
+                                                className="Auth__file"
+                                                id="keystore"
+                                                type="file"
+                                                onChange={this.handleImport}
+                                                accept=".json"
+                                            />
+                                            <p className="Auth__fileName">
+                                            {keystoreName}
+                                            </p>
+                                            </InputGroup>
+                                        </FormGroup>
+                                        <FormGroup>
+                                            <InputGroup
+                                            className={classnames("input-group-alternative", {
+                                                "input-group-focus": this.state.passwordFocus
+                                            })}
+                                            >
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                <i className="tim-icons icon-key-25" />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input
+                                                id="input-password"
+                                                name="password"
+                                                placeholder="Password"
+                                                type="password"
+                                                onChange={this.handleChange}
+                                                onFocus={e => this.setState({ passwordFocus: true })}
+                                                onBlur={e => this.setState({ passwordFocus: false })}
+                                            />
+                                            </InputGroup>
+                                        </FormGroup>
+                                        </Fragment>
+                                        )
+                                        : (
+                                        <Fragment>
+                                        <FormGroup>
+                                            <InputGroup>
+                                            <InputGroupAddon addonType="prepend">
+                                                <InputGroupText>
+                                                <i className="tim-icons icon-lock-circle" />
+                                                </InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input
+                                                id="input-privateKey"
+                                                name="privateKey"
+                                                placeholder="privateKey"
+                                                type="privateKey"
+                                                onChange={this.handleChange}
+                                                onFocus={e => this.setState({ passwordFocus: true })}
+                                                onBlur={e => this.setState({ passwordFocus: false })}
+                                            />
+                                            </InputGroup>
+                                        </FormGroup>
+                                        </Fragment>
+                                        ) 
+                                    }
+
+                                    <p className="Auth__keystoreMsg">{keystoreMsg}</p>
+                                    <p className="Auth__toggleAccessButton" onClick={this.toggleAccessType}>
+                                    {accessType === 'privateKey'
+                                        ? 'Want to login with keystore? (click)'
+                                        : 'Want to login with privatekey? (click)'
+                                    }
+                                    </p>
+                                    <FormGroup check className="mt-3">
+                                    <Label check>
+                                        <Input defaultChecked type="checkbox" />
+                                        <span className="form-check-sign" />
+                                    Remember me!
+                                    </Label>
+                                    </FormGroup>
+                                    <div className="text-center">
+                                    <Button className="my-4" color="success" type="button" onClick={this.handleLogin}>
+                                            Login
+                                    </Button>
+                                    </div>
+                                </Fragment>
+                                </Form>
+                            </div>
+                            </Modal>
+                        </Col>
+                        {/* wallet sign up part */}
+                        <Col className="text-center">
+                        <Button color="primary" onClick={() => this.toggleModal("formModal2")}>Sign up</Button>
+                        <Modal
+                    modalClassName="modal-black"
+                    isOpen={this.state.formModal2}
+                    toggle={() => this.toggleModal("formModal2")}
+                    >
+                        <div className="modal-header justify-content-center">
+                        <Button className="close" onClick={() => this.toggleModal("formModal2")}>
+                            <i className="tim-icons icon-simple-remove text-white" />
+                        </Button>
+                        <div className="text-muted text-center ml-auto mr-auto">
+                            <h3 className="mb-0">Sign up </h3>
+                        </div>
+                        </div>
+                        <div className="modal-body">
+                            <div className="btn-wrapper text-center"></div>
+                            <Form role="form">
+                                <FormGroup className="mb-3">
+                                    <InputGroup>
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="tim-icons icon-lock-circle" />
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input
+                                        className="SignupForm__input"
+                                        placeholder="Generate Private Key to Sign up"
+                                        value={privateKey || ''}
+                                        label="Private key"
+                                        readOnly
+                                    />
+                                    </InputGroup>
+                                    <div className="text-center">
+                                        <Button
+                                        color="primary"
+                                        className="SignupForm__button"
+                                        title="Generate Private key"
+                                        onClick={this.generatePrivateKey}
+                                        >Generate Private key
+                                        </Button>
+                                    </div>
+                                </FormGroup>
+                            </Form>
+                        </div>
+                    </Modal>
+                    </Col>  
+                    </Row>
+                    <div className="space-50"></div>
+                </Form>
+            </CardBody>
+        </Card>
+                
         );
     }
 }
