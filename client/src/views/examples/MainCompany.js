8388a3f75822c786ac193afecb2bf707199b0175
Yunjoung808 <s_holmes25@naver.com>
2021-04-07 20:44:07 +0900

    pay,getUserInfo,decrypt and edit modal

--- client/src/views/examples/MainCompany.js
+++ client/src/views/examples/MainCompany.js
@@ -51,7 +51,9 @@ class MainCompany extends React.Component {
         modalDemo: false,
         userInfo:[],
         encData : '',
-        decData : ''
+        decData : '',
+        isBool:'true',
+        receipt:''
       };
     }
    
@@ -60,7 +62,6 @@ class MainCompany extends React.Component {
           modalDemo: !this.state.modalDemo
       });
   }
-
   componentDidMount() {
     document.body.classList.toggle("index-page"); 
   }
@@ -99,10 +100,8 @@ class MainCompany extends React.Component {
     let userSeq = user_id.replace(/[^0-9]/g,'');
     userContract.methods.getUserInfo(userSeq)
                         .call()
-                        .then(
-                          res => this.setState({ encData : res})
-                          
-                          )
+                        .then(res => this.setState({ encData : res}))
+                        
   }
 
   decryptUserData = () => {
@@ -112,24 +111,34 @@ class MainCompany extends React.Component {
     console.log("암호 키 :", key)
     let decData = this.decrypt(data, key);
     console.log("복호화 데이터 (암호 데이터 + 암호 키) :", decData)
+    this.setState({decData:decData})
+
+    //결제창 닫아주기
+    this.setState({modalDemo: !this.state.modalDemo});
   }
 
   //Company->DM_Plus 결제
   sendToken = () => {
-    //결제창 닫아주기
-    this.setState({modalDemo: !this.state.modalDemo});
-    //검색창 클린
+    this.setState({isBool : false})
     this.setState({searchKeyword:''})
+
     //토큰 보내기
     const feePayer = caver.klay.accounts.wallet.add('0x2f1c41403a47679d6a152bb6edf610888febbefb31db1601fc2bc6c45880b1a8'); //DM_Plus 지갑 주소
       rewardContract.methods.transfer(feePayer.address, 20).send({
         from: feePayer.address, 
         gas: '2500000'
-      }).then(function(receipt){
-        alert("결제 완료 :"+receipt.transactionHash)
-      })
-      this.getInfo();
+      }).then(res => this.setState({receipt:res.transactionHash}))
+
+    this.getInfo();
   }
+
+  onClick = (e) => {
+    e.preventDefault(); 
+    var scope1 = 'https://baobab.scope.klaytn.com/tx/';
+    var scope2 = this.state.receipt;
+    var scope3 = '?tabId=eventLog';
+    window.location.href=scope1+scope2+scope3;
+}
 
   //로그인된 wallet address 
   getWallet = () => {
@@ -147,160 +156,141 @@ class MainCompany extends React.Component {
   }
 
   render() {
-    // let Items =  this.state.userInfo.map(item => {
-    //   if (item._id ==='_id') return( <></>)
-    //   return(
-    //     <Col className="mt-5 mt-sm-0" sm="3" xs="6">
-    //       <div className="card-profile card">
-    //         <div className="card-body">
-    //             <hr className="line-primary"></hr>
-    //               <table className="tablesorter table">
-    //                 <tbody>
-    //                   <tr>
-    //                     <td className="text-left" >
-    //                       <i className="tim-icons icon-bag-16  text-primary" ></i> &nbsp;
-    //                       <p className="category text-primary d-inline">Auth</p>
-    //                     </td>
-    //                     <td className="text-right">{item.name}</td>
-    //                     <td className="text-right">{item.email} email</td>  
-    //                     <td className="text-right">{item.googleId} googleId</td> 
-    //                     <td className="text-right">{item._id} userSeq</td> 
-    //                   </tr>
-    //                 <tr>
-    //                   <td className="text-left">
-    //                   </td>
-    //                   <td className="text-right">
-    //                     <Button className="btn-round btn-sm" color="primary" type="button" Link tag={Link} to="/register-page"
-    //                             onClick={(e) => {e.preventDefault(); window.location.href='/register-page?index='+item._id;}}>
-    //                          <i className="tim-icons icon-minimal-right"/>
-    //                     </Button>
-    //                   </td>
-    //                 </tr>
-    //               </tbody>
-    //             </table>
-    //           </div>
-    //       </div>
-    //   </Col>
-    // )});
+    var { isBool } = this.state;
     var walletInstance = this.getWallet();
     if (walletInstance) { 
       return (
         <>
           <div className="wrapper">
             <div className="page-header">
-            <div className="page-header-image" />
-              <div className="content">
-              <CompanyNavbar />
-              <img alt="..." className="path" src={require("assets/img/blob.png")} />
-                <Container> 
-                <Row>
-                  <Col className="item"><hr className="line-success"></hr></Col>
-                  <div className="space-50"></div>
-                </Row>
-                <Card>
-                    <CardBody>
-                    <form onSubmit={this.handleFormSubmit}>
-                        <FormGroup>
-                            <Row>
+              <div className="page-header-image" />
+                <div className="content">
+                  <CompanyNavbar />
+                  <img alt="..." className="path" src={require("assets/img/blob.png")} />
+                  <Container> 
+                    <Row>
+                      <Col className="item"><hr className="line-success"></hr></Col>
+                      <div className="space-50"></div>
+                    </Row>
+                    <Card>
+                      <CardBody>
+                        <form onSubmit={this.handleFormSubmit}>
+                            <FormGroup>
+                              <Row>
                                 <Col className="col-sm-8">
-                                    <InputGroup>
-                                        <InputGroupText>
-                                            <i className="tim-icons icon-email-85"/>
-                                        </InputGroupText>
-                                        <Input placeholder="Search..." type="text" name="searchKeyword" value={this.state.searchKeyword}
-                                        onChange={this.handleValueChange} className="form-control"/>
-                                    </InputGroup>
+                                  <InputGroup>
+                                    <InputGroupText>
+                                      <i className="tim-icons icon-email-85"/>
+                                    </InputGroupText>
+                                    <Input placeholder="Search..." type="text" name="searchKeyword" value={this.state.searchKeyword}
+                                          onChange={this.handleValueChange} className="form-control"/>
+                                  </InputGroup>
                                 </Col>
                                 <Col className="col-sm-4">
-                                    <Button onClick={this.toggleModalDemo}  className="btn btn-success" type="submit">Search</Button>
-                                      <Modal isOpen={this.state.modalDemo} toggle={this.toggleModalDemo}>
-                                          <div className="modal-header">
-                                            <h3 className="modal-title" id="exampleModalLabel">
-                                              DM_Plus Payment
-                                            </h3>
-                                            <button
-                                              type="button"
-                                              className="close"
-                                              data-dismiss="modal"
-                                              aria-hidden="true"
-                                              onClick={this.toggleModalDemo}
+                                  <Button onClick={this.toggleModalDemo}  className="btn btn-success" type="submit">Search</Button>
+                                    {isBool ?
+                                    <Modal isOpen={this.state.modalDemo} toggle={this.toggleModalDemo}>
+                                        <div className="modal-header text-center">
+                                          <h3 className="modal-title text-center">
+                                            DM_Plus Payment
+                                          </h3>
+                                          <button
+                                            type="button"
+                                            className="close"
+                                            data-dismiss="modal"
+                                            aria-hidden="true"
+                                            onClick={this.toggleModalDemo}
                                             >
-                                              <i className="tim-icons icon-simple-remove" />
-                                            </button>
-                                          </div>
-                                              <ModalBody>
-                                                  <p>Data를 열람하시겠습니까?</p>
-                                                  <Row>
-                                                        <Col className="mt-5 col-12">
-                                                          <Table className=" table">
-                                                              <thead>
-                                                                  <tr>
-                                                                      <th scope="col"><b><p>User</p></b></th>
-                                                                      <th scope="col"><p>Token</p></th>
-                                                                  </tr>
-                                                              </thead>
-                                                              <tbody>
-                                                                  <tr>
-                                                                      <td><p>{this.state.searchKeyword}</p></td>
-                                                                      <td><p>20</p></td>
-                                                                  </tr>
-                                                              </tbody>
-                                                              <tfoot>
-                                                                  <tr>
-                                                                      <th >Total</th>
-                                                                      <th className=" text-default" colSpan="3">20 Token</th>
-                                                                  </tr>
-                                                              </tfoot>
-                                                          </Table>
-                                                        </Col>
-                                                    </Row>
-                                          </ModalBody>
-                                          <ModalFooter className="text-center">
-                                              <Row>
-                                                <Col>
+                                            <i className="tim-icons icon-simple-remove" />
+                                          </button>
+                                        </div>
+                                        <ModalBody className="text-center">
+                                          <p><u>{this.state.searchKeyword}</u>님의  Data를 조회하시겠습니까?</p>
+                                            <Col className="mt-5 col-12">
+                                              <Table reponsive>
+                                                <thead>
+                                                    <tr>
+                                                        <th className="text-default" colSpan="3">User</th>
+                                                        <th className="text-default" colSpan="3">Token</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                  <tr>
+                                                      <td colSpan="3"><p><u>{this.state.searchKeyword}</u></p></td>
+                                                      <td colSpan="3"><p className="text-center">20</p></td>
+                                                  </tr>
+                                                </tbody>
+                                                <tfoot>
+                                                  <tr>
+                                                      <th  colSpan="3"><p className="text-center">Total</p></th>
+                                                      <th className="text-default" colSpan="3">20 Token</th>
+                                                  </tr>
+                                                </tfoot>
+                                              </Table>
+                                            </Col>
+                                            <Row colSpan="3">
+                                              <Col>
                                                 <Button color="secondary" onClick={this.sendToken}>
                                                   결제하기
                                                 </Button>
-                                                </Col>
-                                              </Row>
-                                              
-                                          </ModalFooter>
+                                              </Col>
+                                            </Row>
+                                        </ModalBody>
                                       </Modal>
-                                </Col>
-                            </Row>
-                        </FormGroup>
-                      </form>
-                    </CardBody>
-                </Card>
-                <Row>
-                  <Col className="item"><hr className="line-success"></hr></Col>
-                  <div className="space-50"></div>
-                </Row>
+                                      :<Modal isOpen={this.state.modalDemo} toggle={this.toggleModalDemo}>
+                                        <div className="modal-header text-center">
+                                          <h3 className="modal-title text-center " id="exampleModalLabel">
+                                            DM_Plus Payment
+                                          </h3>
+                                        </div>
+                                        <ModalBody className="text-center">
+                                            <p>결제가 완료되었습니다.</p> 
+                                            <div><p>{this.state.receipt}</p></div>
+                                            <Button color="success" onClick={this.onClick}>Go To Klaytn Scope  <i className="tim-icons icon-minimal-right"/></Button>
+                                            <Row colSpan="3">
+                                              <Col>
+                                                <Button color="secondary" onClick={this.decryptUserData}>
+                                                  닫기
+                                                </Button>
+                                              </Col>
+                                            </Row>
+                                        </ModalBody>
+                                      </Modal>}
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                          </form>
+                        </CardBody>
+                    </Card>
+                    <Row>
+                      <Col className="item"><hr className="line-success"></hr></Col>
+                      <div className="space-50"></div>
+                    </Row>
                     <Card>
                       <CardBody>
-                      <Row>
-                        <Col className="align-self-center col-md-3">
-                        <Badge color="success">My Wallet Address</Badge>
-                        </Col>
-                        <Col className="align-self-center col-md-8">
-                        <p className="text-neutral"><b>{walletInstance.address}</b></p>
-                        </Col>
+                        <Row>
+                          <Col className="align-self-center col-md-3">
+                            <Badge color="success">My Wallet Address</Badge>
+                          </Col>
+                          <Col className="align-self-center col-md-8">
+                            <p className="text-neutral"><b>{walletInstance.address}</b></p>
+                          </Col>
                         </Row>
                         {this.state.encData}
-                        {/* {Items} */}
+                        {this.state.decData}
                       </CardBody>
                     </Card>
                   </Container>
                 </div>
               </div>
-              <div class="space-70"></div>
-            <div class="space-70"></div>  
-          <Footer/>
-        </div>
-      </>
-      );
-    }
+            <div class="space-70"></div>
+          <div class="space-70"></div>  
+        <Footer/>
+      </div>
+    </>
+    );
   }
+}
 }
 
 export default MainCompany;
