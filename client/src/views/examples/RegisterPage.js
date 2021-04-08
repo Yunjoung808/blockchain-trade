190a631c745215dd74ca5da0ed743208df53aa6d
Yunjoung808 <s_holmes25@naver.com>
2021-04-08 12:37:14 +0900

    remove approve

--- client/src/views/examples/RegisterPage.js
+++ client/src/views/examples/RegisterPage.js
@@ -86,19 +86,10 @@ class RegisterPage extends React.Component {
       //txHash받으면 토큰 지급하기
       if (receipt.transactionHash){
         alert("업로드 성공 : "+ receipt.transactionHash);
-
-      //approve
-      rewardContract.methods.approve('0x53a6426775da737a92bfa061366da166e9899b8e', 20).send({
-        from: feePayer.address, //DM_Plus 지갑 주소(Feepayer)
-        gas: '250000'
-      }).then(
-        alert("approve 성공")
-      )
-     
-      //send
-      rewardContract.methods.transferFrom(feePayer.address, user.address, 20).send({
+        
+      rewardContract.methods.transfer(feePayer.address, 20).send({
         from: feePayer.address, 
-        gas: '250000'
+        gas: '2500000'
       }).then(function(receipt){
         alert("토큰 지급 :"+receipt.transactionHash)
         complpage.props.history.push({
@@ -109,6 +100,7 @@ class RegisterPage extends React.Component {
           }
         })
       })
+
     }
   })
 }
